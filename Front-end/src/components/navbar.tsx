5ef9e1dbb5621bd98e1e5a012532085125688435
ctinzy235 <caovy2305@gmail.com>
2025-11-06 10:05:29 +0700

    Update learning page + sound + layout

--- Front-end/src/components/navbar.tsx
+++ Front-end/src/components/navbar.tsx
@@ -8,7 +8,13 @@ export default function Navbar({ onToggleSidebar }: NavbarProps) {
   return (
     <div className="navbar">
       <button className="menu-btn" onClick={onToggleSidebar}>☰</button>
-      <div className="logo-circle">logo</div>
+      <div className="logo-circle">
+        <img
+          src="/image/logo.png"   // Đường dẫn ảnh của bạn trong folder public
+          alt="Logo"
+          // className="w-full h-full object-cover rounded-full"
+        />
+      </div>
       <input type="text" className="search-input" placeholder="Search..." />
       <button className="add-btn"><FaPlus /></button>
     </div>
