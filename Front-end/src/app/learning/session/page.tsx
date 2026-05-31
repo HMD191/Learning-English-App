dcef1033955da83ec8f304bf634683c25bd1f3ce
ctinzy235 <caovy2305@gmail.com>
2026-05-31 09:02:34 +0700

    fix deploy

--- Front-end/src/app/learning/session/page.tsx
+++ Front-end/src/app/learning/session/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
 import LearningPage from "@/components/learning/LearningPage";
+import LoadingLearning from "@/components/learning/LoadingLearning";
 
 export default function LearningSessionPage() {
-  return <LearningPage />;
+  return (
+    <Suspense fallback={<LoadingLearning />}>
+      <LearningPage />
+    </Suspense>
+  );
 }
